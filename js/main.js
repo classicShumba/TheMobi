652f110905e6079a3bbd5bf698bdcbd0e3ba1a06
[Allan Shumba] <[questykings@gmail.com]>
2023-04-04 14:49:46 +0200

    updated links

--- js/main.js
+++ js/main.js
@@ -1,36 +1,34 @@
-const nav = document.querySelector('.nav')
-    burger = document.querySelector('.burger')
-    menu = document.querySelector('.menu')
-    body = document.querySelector('body')
-    menuItem = document.querySelectorAll('.menu-item')
-    console.log(menuItem)
+const nav = document.querySelector(".nav");
+burger = document.querySelector(".burger");
+menu = document.querySelector(".menu");
+body = document.querySelector("body");
+links = document.querySelectorAll(".nav-link");
 
+burger.addEventListener("click", () => {
+  nav.classList.toggle("open");
+  menu.classList.toggle("menu-open");
+});
 
-    burger.addEventListener('click', () =>{
-        nav.classList.toggle('open');
-        menu.classList.toggle('menu-open')
+body.addEventListener("click", (e) => {
+  let clickedEl = e.target;
+  console.log(clickedEl);
+  if (
+    !clickedEl.classList.contains("burger") &&
+    !clickedEl.classList.contains("menu-items") &&
+    !clickedEl.classList.contains("line")
+  ) {
+    nav.classList.remove("open");
+    menu.classList.remove("menu-open");
+  }
+});
+
+if (links.length) {
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      links.forEach((link) => {
+        link.classList.remove("active");
+      });
+      link.classList.add("active");
     });
-
-    body.addEventListener('click', e =>{
-        let clickedEl = e.target
-        console.log(clickedEl)
-        if (!clickedEl.classList.contains('burger') && !clickedEl.classList.contains('menu-items') && !clickedEl.classList.contains('line')){
-            nav.classList.remove('open')
-            menu.classList.remove('menu-open')
-        }
-    })
-
-    for (let i = 0; i < menuItem.length; i++){
-        menuItem[i].addEventListener('click', () =>{
-            let current = document.querySelector('active');
-
-            if (current.length > 0){
-                current[0].className = current[0].classList.add(' active')
-            }
-
-            this.classList += 'active'
-        })
-    }
-    
-
-    
+  });
+}
